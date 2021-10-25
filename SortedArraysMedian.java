896a63e8e85ae03f54da9ad19723eecf76435505
Hitesh Dholaria <hitesh.dholaria@gmail.com>
2021-10-25 14:10:45 +0530

    Update SortedArraysMedian.java

--- SortedArraysMedian.java
+++ SortedArraysMedian.java
@@ -9,87 +9,97 @@ public class SortedArraysMedian {
         return 0.0;
     }
 
-    // Returns how many numbers in the given sorted array[from, to] are greater than the given number.
+    // Returns how many numbers in the given sorted array are greater than the given number.
     public static int greaterNumbers(int[] array, int number) {
         return greaterNumbers(array, number, 0, array.length - 1);
     }
 
-    // Returns how many numbers in the given sorted array[from, to] are greater than the given number.
-    public static int greaterNumbers(int[] array, int number, int from, int to) {
+    // Returns how many numbers in the given sorted array in [arrayStart, arrayEnd] range are greater than the given number.
+    public static int greaterNumbers(int[] array, int number, int arrayStart, int arrayEnd) {
+        return greaterNumbers(array, number, arrayStart, arrayEnd, arrayStart, arrayEnd);
+    }
+
+    // Returns how many numbers in the given sorted array in [arrayStart, arrayEnd] range are greater than the given number.
+    private static int greaterNumbers(int[] array, int number, int arrayStart, int arrayEnd, int from, int to) {
         if (from < 0 || to < 0 || from > array.length - 1 || to > array.length - 1) {
             return 0;
         }
         if (from == to) {
             if (array[from] <= number) {
-                return array.length - from - 1;
+                return arrayEnd + 1 - from - 1;
             } else {
-                return array.length - from;
+                return arrayEnd + 1 - from;
             }
         }
         if (number > array[to]) {
-            return array.length - to - 1;
+            return arrayEnd + 1 - to - 1;
         }
         if (number < array[from]) {
-            return array.length - from;
+            return arrayEnd + 1 - from;
         }
         if (from < to) {
             int mid = (from + to) / 2;
             if (array[mid] <= number) {
-                return greaterNumbers(array, number, mid + 1, to);
+                return greaterNumbers(array, number, arrayStart, arrayEnd, mid + 1, to);
             } else {
-                return greaterNumbers(array, number, from, mid - 1);
+                return greaterNumbers(array, number, arrayStart, arrayEnd, from, mid - 1);
             }
         } else {
             return 0;
         }
     }
 
-    // Returns how many numbers in the given sorted array[from, to] are smaller than the given number.
+    // Returns how many numbers in the given sorted array are smaller than the given number.
     public static int smallerNumbers(int[] array, int number) {
         return smallerNumbers(array, number, 0, array.length - 1);
     }
 
-    // Returns how many numbers in the given sorted array[from, to] are smaller than the given number.
-    public static int smallerNumbers(int[] array, int number, int from, int to) {
+    // Returns how many numbers in the given sorted array in [arrayStart, arrayEnd] range are smaller than the given number.
+    public static int smallerNumbers(int[] array, int number, int arrayStart, int arrayEnd) {
+        return smallerNumbers(array, number, arrayStart, arrayEnd, arrayStart, arrayEnd);
+    }
+
+    // Returns how many numbers in the given sorted array in [arrayStart, arrayEnd] range are smaller than the given number.
+    private static int smallerNumbers(int[] array, int number, int arrayStart, int arrayEnd, int from, int to) {
         if (from < 0 || to < 0 || from > array.length - 1 || to > array.length - 1) {
             return 0;
         }
         if (from == to) {
             if (array[from] < number) {
-                return to + 1;
+                return to + 1 - arrayStart;
             } else {
-                return to;
+                return to - arrayStart;
             }
         }
         if (number < array[from]) {
-            return from;
+            return from - arrayStart;
         }
         if (number > array[to]) {
-            return to + 1;
+            return to + 1 - arrayStart;
         }
         if (from < to) {
             int mid = (from + to) / 2;
             if (array[mid] >= number) {
-                return smallerNumbers(array, number, from, mid - 1);
+                return smallerNumbers(array, number, arrayStart, arrayEnd, from, mid - 1);
             } else {
-                return smallerNumbers(array, number, mid + 1, to);
+                return smallerNumbers(array, number, arrayStart, arrayEnd, mid + 1, to);
             }
         } else {
             return 0;
         }
     }
 
-    // Returns how many numbers in the given sorted array[from, to] are equal to the given number.
+    // Returns how many numbers in the given sorted array are equal to the given number.
     public static int equalNumbers(int[] array, int number) {
         return equalNumbers(array, number, 0, array.length - 1);
     }
 
-    // Returns how many numbers in the given sorted array[from, to] are equal to the given number.
-    public static int equalNumbers(int[] array, int number, int from, int to) {
-        if (array.length == 0) {
+    // Returns how many numbers in the given sorted array in [arrayStart, arrayEnd] range are equal to the given number.
+    public static int equalNumbers(int[] array, int number, int arrayStart, int arrayEnd) {
+        if (array.length == 0 || arrayStart > arrayEnd) {
             return 0;
         }
-        return array.length - smallerNumbers(array, number) - greaterNumbers(array, number);
+        return arrayEnd - arrayStart + 1 - smallerNumbers(array, number, arrayStart, arrayEnd) - greaterNumbers(array, number, arrayStart, arrayEnd);
     }
 
 }
